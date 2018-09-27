ce7a12402b0b24de64972241552c3f3448551294
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2018-09-27 17:12:26 -0500

    Fixes to find_road

--- src/find_road.h
+++ src/find_road.h
@@ -1,5 +1,6 @@
 #ifndef FIND_ROAD_H
 #define FIND_ROAD_H
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -10,7 +11,11 @@
 
 static const std::string OPENCV_WINDOW = "Image window";
 
-
+/**
+   Class that subscribes to the image stream from the camera on the ackermann vehicle,
+   and implements some basic computer vision methods to find the road ahead. 
+   The class is header-file only. You only need to include the header file in your code.
+ */
 class RoadFinder
 {
   cv::Point2f middle_of_road_;
@@ -18,15 +23,38 @@ class RoadFinder
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
-  cv::Mat thr, gray;
-  
-public:
-  RoadFinder()
-    : it_(nh_)
+  cv::Mat thr, gray, hsv, edge;
+
+  std::vector<cv::Vec4i> lines_; // To hold lines found by Hough transform
+
+ public:
+ RoadFinder()
+   : it_(nh_)
   {
-    // Subscrive to input video feed and publish output video feed
+    // Temporary values 
+    middle_of_road_.x = 400;
+    middle_of_road_.y = 400;
+
+    // Subscribe to input video feed and publish output video feed
     image_sub_ = it_.subscribe("/ackermann_vehicle/camera1/image_raw", 1,
-      &RoadFinder::imageCb, this);
+      &RoadFinder::imageCallback, this);
+    image_pub_ = it_.advertise("/road_finder/output_video", 1);
+    
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+
+
+ RoadFinder(const char* topic_name)
+   : it_(nh_)
+  {
+    // Temporary values 
+    middle_of_road_.x = 400;
+    middle_of_road_.y = 400;
+
+    // Subscribe to input video feed and publish output video feed
+    image_sub_ = it_.subscribe(topic_name, 1,
+      &RoadFinder::imageCallback, this);
     image_pub_ = it_.advertise("/road_finder/output_video", 1);
 
     cv::namedWindow(OPENCV_WINDOW);
@@ -37,15 +65,57 @@ public:
     cv::destroyWindow(OPENCV_WINDOW);
   }
 
-  int width(){
-    return thr.cols;
+  int imageWidth(){
+    if (thr.empty()) { return 800;} // Temporary value
+    else {
+      return thr.cols;
+    }
   }
 
-  cv::Point2f& get_midpoint() {
+  int imageHeight(){
+    if (thr.empty()) { return 600; } // Temporary value
+    else {
+      return thr.rows;
+    }
+  }
+
+  cv::Point2f& getMidpoint() {
     return middle_of_road_;
   }
-  
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+
+  /**
+     Returns the number of continuous white pixels along the middle column of the thresholded image
+     starting from the bottom of the image. 
+  */
+  int freeRoadAhead() {
+    if (thr.empty()) {
+      return 0;
+    }
+    else {
+      // Get mid column of thresholded image
+      int horizon = imageHeight() / 2;
+      int midcol = imageWidth() / 2;
+      cv::Mat mid_line = thr.col(midcol);
+      
+      int k = imageHeight() - 1;
+
+
+      while ( mid_line.at<uchar>(k) == 255 and k>horizon) {k--;}
+      return imageHeight() - k;
+    }
+  }
+
+  /**
+     Returns the lines found in the image 
+   */
+  std::vector<cv::Vec4i>& getLines() {
+    return lines_; 
+  }
+
+  /**
+     Callback function analyzing the incoming images
+  */
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
     try
@@ -59,32 +129,48 @@ public:
     }
 
     // Find white part of image. Determine centroid and draw circle at centroid
- 
+    // Make sure to ignore the marks in the middle of the road. Since the white contains also
+    // the orange of the mid stripes, convert to color space 
     // convert image to grayscale
-    cv::cvtColor( cv_ptr->image, gray, cv::COLOR_BGR2GRAY );
- 
+    cv::cvtColor( cv_ptr->image, hsv, cv::COLOR_BGR2HSV);
+
+    cv::inRange( hsv, cv::Scalar(0, 0, 200), cv::Scalar(60, 255, 255), thr );
+    //cv::cvtColor( cv_ptr->image, gray, cv::COLOR_BGR2GRAY );
     // convert grayscale to binary image
-    cv::threshold( gray, thr, 200,255,cv::THRESH_BINARY );
+    //cv::threshold( gray, thr, 180,255,cv::THRESH_BINARY );
  
     // find moments of the image
     cv::Moments m = cv::moments(thr,true);
     middle_of_road_.x = m.m10/m.m00;
     middle_of_road_.y =  m.m01/m.m00;
-    
+
+    cv::Canny(thr, edge, 50, 200, 3 ); // detect edges
+    cv::HoughLinesP(edge, lines_, 1, CV_PI/180, 50, 50, 10 ); // detect lines
+
     // coordinates of centroid
     //cout<< Mat(p)<< endl;
  
-    // show the image with a point mark at the centroid
+    // show the image with a point mark at the centroid, and detected lines
+    
     if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
       cv::circle(cv_ptr->image, middle_of_road_, 10, CV_RGB(255,0,0));
 
+       // Draw the lines
+    for( int i = 0; i < lines_.size(); i++ )
+    {
+      cv::Vec4i l = lines_[i];
+      cv::line( cv_ptr->image, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(120,0,200), 3, cv::LINE_AA);
+    }
+
     // Update GUI Window
+    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::waitKey(3);
 
     // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+    // image_pub_.publish(cv_ptr->toImageMsg());
   }
-};
+
+  };
 
 #endif 
--- src/stay_on_road_test.cpp
+++ src/stay_on_road_test.cpp
@@ -5,8 +5,13 @@ int main(int argc, char** argv)
 {
   float speed = 0.7;  
   float Ka = 1.0/200;
+  float max_speed = 2;
+  float min_speed = 0.1;
+  
   if (argc > 1) { speed = atof(argv[1]); }
   if (argc > 2) { Ka = atof(argv[2]); }
+  if (argc > 3) { max_speed = atof(argv[3]); }
+  if (argc > 4) { min_speed = atof(argv[4]); }
 
 
   ros::init(argc, argv, "find_road");
@@ -17,11 +22,12 @@ int main(int argc, char** argv)
   while(ros::ok()) {
     // The speed is constant, but the steering angle is proportional
     //to the distance of the middle of the road from the middle of the image
-
+ 
     ros::spinOnce();
-    float error = rf.width()/2 - rf.get_midpoint().x; // Error in number of pixels
-    ROS_INFO("Sending command speed=%f, steering_angle=%f", speed, error*Ka);
-    am.go(speed, error*Ka);
+    float error = rf.imageWidth()/2 - rf.getMidpoint().x; // Error in number of pixels
+    float speed_command = speed; // Here you could do something smarter
+    ROS_INFO("Sending command speed=%f, steering_angle=%f", speed_command, error*Ka);
+    am.go(speed_command, error*Ka);
     rate.sleep();
   }
   return 0;
